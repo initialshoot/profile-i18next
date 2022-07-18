34b003cab8e2ebb9a5c20d4d066b7b82462e4917
Omar_asc <initialshoot@outlook.com>
2022-07-18 14:23:24 -0500

    Feat: add funcionability to technologies buttons

--- src/App.js
+++ src/App.js
@@ -32,35 +32,49 @@ function App() {
           </div>
 
           <div className='technologies'>
-          
-            <button>
-              <FaReact /> React
-            </button>
 
-            <button>
-              <FaAngular /> Angular
-            </button>
-
-            <button>
-              <DiIonic /> Ionic
-            </button>
-
-            <button>
-              <DiMongodb /> MongoDB
-            </button>
-
-            <button>
-              <DiGoogleCloudPlatform /> Google Cloud
-            </button>
-
-            <button>
-              <FaPython /> Python
-            </button>
-
-            <button>
-              <FaJava /> Java
-            </button>
-
+            <a href='https://es.reactjs.org/' target='_blank'>
+              <button>
+                <FaReact /> React
+              </button>
+            </a>
+            
+            <a href='https://angular.io/' target='_blank'>
+              <button>
+                <FaAngular /> Angular
+              </button>
+            </a>
+            
+            <a href='https://ionicframework.com/' target='_blank'>
+              <button>
+                <DiIonic /> Ionic
+              </button>
+            </a>
+            
+            <a href='https://www.mongodb.com/es' target='_blank'>
+              <button>
+                <DiMongodb /> MongoDB
+              </button>
+            </a>
+            
+            <a href='https://cloud.google.com/' target='_blank'>
+              <button>
+                <DiGoogleCloudPlatform /> Google Cloud
+              </button>
+            </a>
+            
+            <a href='https://www.python.org/' target='_blank'>
+              <button>
+                <FaPython /> Python
+              </button>
+            </a>
+            
+            <a href='https://www.java.com/es/' target='_blank'>
+              <button>
+                <FaJava /> Java
+              </button>
+            </a>
+            
           </div>
 
         </div>
